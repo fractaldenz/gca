351f5a0845ff2fc82d53761ce387fd0ee66619ef
fractaldenz <augusto.domingo@fractaldev.com>
2015-10-26 14:33:48 +0800

    first commit

--- index.php
+++ index.php
@@ -2,5 +2,13 @@
 <html <?php language_attributes(); ?>>
 <head>
  <title><?php bloginfo('name'); ?></title>
+ <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" />
+ <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.css" />
+ <link type="text/css" rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+ <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
+ <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.js"></script>
 </head>
+<body <?php body_class(); ?>>
+    
+</body>
 </html>
